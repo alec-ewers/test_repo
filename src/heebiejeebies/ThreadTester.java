8fc67e08c296f5d970186e0706c10b741e12d87a
alec.ewers <mr_penumbra@Ravnen-PC>
2018-05-03 12:27:41 -0700

    Created ThreadTester class to test threads.

---
+++ src/heebiejeebies/ThreadTester.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package heebiejeebies;
+
+/**
+ *
+ * @author mr_penumbra
+ * @Override run() the run method of thread
+ */
+public class ThreadTester extends Thread {
+    
+    @Override
+    public void run() {
+        System.out.println("I did a thing.");
+    }
+    
+}
