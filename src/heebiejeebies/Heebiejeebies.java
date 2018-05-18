4e77c0e2a8e0f865fe9356c60b6490077b9614a8
alec.ewers <mr_penumbra@Ravnen-PC>
2018-05-18 09:03:03 -0700

    Placed everything in the main class for simplicity

--- src/heebiejeebies/Heebiejeebies.java
+++ src/heebiejeebies/Heebiejeebies.java
@@ -34,7 +34,53 @@ public class Heebiejeebies {
         
         
     }
+}
+
+class ComponentTester extends JComponent {
     
+    @Override
+    public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        
+        Image ted = Toolkit.getDefaultToolkit().getImage("backblock.png");
+        Image jeff = Toolkit.getDefaultToolkit().getImage("char.png");
+        
+        for (int I = 0; I < 640/64 + 1; I++) {
+            for (int J = 0; J < 480/64 + 1; J++) {
+                g2.drawImage(ted, I * 64, J * 64, this);
+            }
+        }
+        
+        g2.drawImage(jeff, 75, 75, this);
+
+        g2.finalize();
+        
+        //g2.drawImage(ted, 10, 10, this);
+        //g2.finalize();
+    }
     
+    private int MessageX = 75;
+    private int MessageY = 100;
+
+}
+
+class WordFrame extends JFrame {
     
+    public WordFrame(int init) {
+        setTitle("I hate this parent/child heirarchy crap.");
+        setSize(640, 480);
+        
+        if (init % 2 == 0) {
+            CharTester test = new CharTester();
+            add(test);
+        }else if (init % 2 == 1) {
+            ComponentTester tester = new ComponentTester();
+            add(tester);
+        }
+        //ComponentTester tester = new ComponentTester();
+        ////CharTester test = new CharTester();
+        //add(tester);
+        ////add(test);
+    }
+
 }
