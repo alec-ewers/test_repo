34bd040179cc1488369cc7bde373bb922b609f30
alec.ewers <mr_penumbra@Ravnen-PC>
2018-05-20 10:16:24 -0700

    added a whole bunch of stuff based on albert's mods to world, but it doesn't so much work yet.

--- src/heebiejeebies/Heebiejeebies.java
+++ src/heebiejeebies/Heebiejeebies.java
@@ -13,28 +13,51 @@ import java.awt.event.*;
  * @author alec.ewers
  */
 public class Heebiejeebies {
-
+    
     /**
      * @param args the command line arguments
+     * @throws java.lang.InterruptedException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
+        WordFrame test = new WordFrame();
+        test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        test.setVisible(true);
         
-      
+        frameThread FThread = new frameThread(test);
         
-        EventQueue.invokeLater(new Runnable() {
+        while (1==1) {
+            FThread.sleep(100);
             
-            @Override
-            public void run() {
-                WordFrame test = new WordFrame(3);
-                test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                test.setVisible(true);
-            } 
-            
-        });
-       
-        
-        
+            FThread.run();
+        }
     }
+}
+
+class frameThread extends Thread {
+    
+    private WordFrame hold;
+    private int X;
+    private int oldX;
+    private int Y;
+    private int oldY;
+    
+    public frameThread(WordFrame frame) {
+        hold = frame;
+    }
+    
+    @Override
+    public void run() {
+        X = ComponentTester.getXPos();
+        Y = ComponentTester.getYPos();
+        
+        if (X != oldX || Y != oldY) {
+            hold.repaint();
+        }
+        
+        oldX = X;
+        oldY = Y;
+    }
+    
 }
 
 class ComponentTester extends JComponent {
@@ -60,51 +83,81 @@ class ComponentTester extends JComponent {
         //g2.finalize();
     }
     
-    public void setX(int x) {
+    //turns out that the JComponent class 
+    
+    public static void setXPos(int x) {
         X = x;
     }
     
-    public void setY(int y) {
+    public static int getXPos() {
+        return X;
+    }
+    
+    public static void setYPos(int y) {
         Y = y;
     }
     
-    private int X = 75;
-    private int Y = 100;
+    public static int getYPos() {
+        return Y;
+    }
+    
+    private static int X = 75;
+    private static int Y = 100;
 
 }
 
 class WordFrame extends JFrame {
     
-    public WordFrame(int init) {
+    public WordFrame() {
+        dispose();
+        
         setTitle("I hate this parent/child heirarchy crap.");
         setSize(640, 480);
         
-        if (init % 2 == 0) {
-            CharTester test = new CharTester();
-            add(test);
-        }else if (init % 2 == 1) {
-            ComponentTester tester = new ComponentTester();
-            add(tester);
-        }
-        //ComponentTester tester = new ComponentTester();
-        ////CharTester test = new CharTester();
-        //add(tester);
-        ////add(test);
+        CharTester test  = new CharTester();
+        add(test);
+        
     }
-
+    
+    public void handleKeyPush(KeyEvent ke) {
+        int key = ke.getKeyCode();
+        
+        switch (key) {
+            case KeyEvent.VK_LEFT :
+                ComponentTester.setXPos(ComponentTester.getXPos() - 5);
+            case KeyEvent.VK_RIGHT :
+                ComponentTester.setXPos(ComponentTester.getXPos() + 5);
+            case KeyEvent.VK_DOWN :
+                ComponentTester.setYPos(ComponentTester.getYPos() - 5);
+            case KeyEvent.VK_UP :
+                ComponentTester.setYPos(ComponentTester.getYPos() + 5);
+        }
+    } 
 }
 
 class listening implements KeyListener {
     
+    //included for functionality's sake
+    @Override
+    public void keyTyped(KeyEvent ke) {
+    }
+    @Override
+    public void keyReleased(KeyEvent ke) {
+    }
+    
+    //responds to inputs of different types
     @Override
     public void keyPressed(KeyEvent ke) {
         int key = ke.getKeyCode();
         switch (key) {
-            case (key == KeyEvent.VK_LEFT) {
-                
-            }
+            case KeyEvent.VK_LEFT :
+                ComponentTester.setXPos(ComponentTester.getXPos() - 5);
+            case KeyEvent.VK_RIGHT :
+                ComponentTester.setXPos(ComponentTester.getXPos() + 5);
+            case KeyEvent.VK_DOWN :
+                ComponentTester.setYPos(ComponentTester.getYPos() - 5);
+            case KeyEvent.VK_UP :
+                ComponentTester.setYPos(ComponentTester.getYPos() + 5);
         }
-             
-        
     }
 }
