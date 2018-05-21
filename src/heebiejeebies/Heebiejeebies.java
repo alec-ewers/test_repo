f0b5cc4d455a32adac37999ac67449542a07f455
alec.ewers <mr_penumbra@Ravnen-PC>
2018-05-21 10:24:29 -0700

    added text box

--- src/heebiejeebies/Heebiejeebies.java
+++ src/heebiejeebies/Heebiejeebies.java
@@ -34,12 +34,15 @@ class ComponentTester extends JComponent {
         
         Image ted = Toolkit.getDefaultToolkit().getImage("backblock.png");
         Image jeff = Toolkit.getDefaultToolkit().getImage("char.png");
+        Image rob = Toolkit.getDefaultToolkit().getImage("textBox.png");
         
         for (int I = 0; I < 640/64 + 1; I++) {
             for (int J = 0; J < 480/64 + 1; J++) {
                 g2.drawImage(ted, I * 64, J * 64, this);
             }
         }
+        
+        g2.drawImage(rob, 0, 480-150, this);
         
         g2.drawImage(jeff, X, Y, this);
         
@@ -72,6 +75,33 @@ class ComponentTester extends JComponent {
         return Y;
     }
     
+    public static boolean getMenu() {
+        return isMenu;
+    }
+    
+    public static void setMenu(boolean newMenu) {
+        isMenu = newMenu;
+    }
+    
+    public static boolean getSpeaking() {
+        return isSpeaking;
+    }
+    
+    public static void setSpeaking (boolean newTalk) {
+        isSpeaking = newTalk;
+    }
+    
+    public static boolean getNext() {
+        return isNext;
+    }
+    
+    public static void setNext (boolean newFollow) {
+        isNext = newFollow;
+    }
+    
+    private static boolean isMenu = false;
+    private static boolean isNext = false;
+    private static boolean isSpeaking = false;
     private static int X = 75;
     private static int Y = 100;
 
@@ -108,18 +138,30 @@ class listening implements KeyListener {
     public void keyPressed(KeyEvent ke) {
         int key = ke.getKeyCode();
         
-        if (key == KeyEvent.VK_LEFT) {
-            ComponentTester.setXPos(ComponentTester.getXPos() - 5);
-            //System.out.println("We have left-off!");
-        } else if (key == KeyEvent.VK_RIGHT) {
-            ComponentTester.setXPos(ComponentTester.getXPos() + 5);
-            //System.out.println("It's a right-off!");
-        } else if (key == KeyEvent.VK_DOWN) {
-            ComponentTester.setYPos(ComponentTester.getYPos() + 5);
-            //System.out.println("You're getting me down!");
-        } else if (key == KeyEvent.VK_UP) {
-            ComponentTester.setYPos(ComponentTester.getYPos() - 5);
-            //System.out.println("Try to keep up!");
+        if (ComponentTester.getMenu() == false) {
+            if (key == KeyEvent.VK_LEFT) {
+                ComponentTester.setXPos(ComponentTester.getXPos() - 5);
+                //System.out.println("We have left-off!");
+            } else if (key == KeyEvent.VK_RIGHT) {
+                ComponentTester.setXPos(ComponentTester.getXPos() + 5);
+                //System.out.println("It's a right-off!");
+            } else if (key == KeyEvent.VK_DOWN) {
+                ComponentTester.setYPos(ComponentTester.getYPos() + 5);
+                //System.out.println("You're getting me down!");
+            } else if (key == KeyEvent.VK_UP) {
+                ComponentTester.setYPos(ComponentTester.getYPos() - 5);
+                //System.out.println("Try to keep up!");
+            } else if (key == KeyEvent.VK_E) {
+                ComponentTester.setYPos(ComponentTester.getYPos() - 5);
+                //System.out.println("Try to keep up!");
+                ComponentTester.setMenu(true);
+            }
+        } else if (ComponentTester.getMenu() == true) {
+            if (ComponentTester.getSpeaking() == true) {
+                if (key != 0) {
+                    ComponentTester.setNext(true);
+                }
+            } else if (ComponentTester.getNext)
         }
     }
 }
